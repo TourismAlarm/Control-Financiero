06f6c810bb0a1a9052fd8fda554feddba10109f2
Tourism Alarm <jordielias5@gmail.com>
2025-10-09 22:06:27 +0200

    Handle optional autoprefixer dependency

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+const plugins = {
+  tailwindcss: {},
 };
+
+try {
+  // Autoprefixer mejora la compatibilidad CSS, pero lo tratamos como opcional
+  // para evitar fallos de instalación en entornos sin acceso al registro.
+  // eslint-disable-next-line global-require, import/no-extraneous-dependencies
+  plugins.autoprefixer = require('autoprefixer');
+} catch (error) {
+  console.warn('Autoprefixer no disponible; se omiten los prefijos CSS automáticos.');
+}
+
+module.exports = { plugins };
